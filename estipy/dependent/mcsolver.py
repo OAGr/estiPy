a3018ab3a08c5511cac5123b110ccc4c5d5e2663
Ozzie Gooen <Ozzie@BowLabs.org>
2014-06-09 20:51:11 -0700

    changed mcsolver to use numpy

--- estipy/dependent/mcsolver.py
+++ estipy/dependent/mcsolver.py
@@ -1,5 +1,4 @@
-import operator
-
+import numpy
 '''
 If no analytical solution exists, this is here to run a monte carlo
 simulation on a given set of estimations with a given operator.
@@ -14,26 +13,19 @@ IndependentEstimate(10,3))
 Example Output:
 [360,301,297,271,389]
 '''
-import pdb
 def run(operation, n, estimates):
     operations = {"+":_add, "*":_mul, "/":_div, '-':_sub}
     generated = [d.run(n) for d in estimates]
     return operations[operation](generated)
 
 def _add(generated):
-    return reduce(operator.add, generated,0)
+    return numpy.sum(generated, axis=0)
 
 def _mul(generated):
-    return reduce(operator.mul,generated, 1)
+    return numpy.prod(generated, axis=0)
 
 def _div(generated):
-    return reduce(operator.div, generated[1:], generated[0])
+    return generated[0] / _mul(generated[1:])
 
 def _sub(generated):
-    return reduce(operator.sub, generated[1:], generated[0])
-
-def _greater_than(generated):
-    return reduce(operator.gt, generated[1:], generated[0])
-
-def _less_than(generated):
-    return reduce(operator.lt, generated[1:], generated[0])
+    return generated[0] - _add(generated[1:])
